c5f0ebccecd5cfb472c9b08455f5b5ebc302f267
Scott Kelly <scottshamus@gmail.com>
2014-02-12 17:06:55 -0600

    improved logging and error handling

---
+++ README.md
@@ -0,0 +1,9 @@
+bubuntu
+=======
+
+Bash script for creating a bootable ubuntu thumb drive in OSX
+
+Only takes two arguments
+
+1. Disk number of attached thumb drive e.g. N from /dev/diskN
+2. Path to your Ubuntu ISO
--- bubuntu.sh
+++ bubuntu.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
-hdiutil convert -format UDRW -o $3 $2
-diskutil unmountDisk /dev/disk$1
-dd if=$3.dmg of=/dev/rdisk$1 bs=1m
-diskutil eject /dev/disk$1 
+if [ -z "$1" ]
+  then
+    echo "Thumb drive disk number not supplied, please run 'diskutil list'"
+    exit 1
+fi
 
+if [ -z "$2" ]
+  then
+    echo "ISO path not supplied"
+    exit 1
+fi
+
+# the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Current working directory is $DIR"
+
+# the temp directory used, within $DIR
+WORK_DIR=`mktemp -d "$DIR/tempXXXXXX"`
+echo "Created temporary working directory $WORK_DIR"
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$WORK_DIR"
+  echo "Deleted temp working directory $WORK_DIR"
+}
+
+# ensure clean always runs
+trap cleanup EXIT
+
+# convert iso to dmg
+hdiutil convert -format UDRW -o $WORK_DIR/iso.img $2 || exit 1
+
+# renaming just because
+mv $WORK_DIR/iso.img.dmg $WORK_DIR/iso.img
+
+diskutil unmountDisk /dev/disk$1
+
+echo "Creating bootable image on thumb drive"
+
+# copy img over to thumb drive
+dd if=$WORK_DIR/iso.img of=/dev/rdisk$1 bs=1m || exit 1
+
+echo "Completed creation of bootable thumb drive, ejecting"
+
+diskutil eject /dev/disk$1
+
+exit 0
